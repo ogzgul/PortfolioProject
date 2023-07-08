959132ccd4216bf16b68159c81b928bcbdab582b
Oğuz Gül <oguz_gul216@hotmail.com>
2023-07-08 17:41:46 +0300

    ViewComponents created.

---
+++ PortfolioProject/ViewComponents/About/AboutList.cs
@@ -0,0 +1,16 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioProject.ViewComponents.About
+{
+    public class AboutList:ViewComponent
+    {
+        AboutManager _aboutManager = new AboutManager(new EfAboutDal());
+        public IViewComponentResult Invoke()
+        {
+            var listedAbout = _aboutManager.GetAll().Data;
+            return View(listedAbout);
+        }
+    }
+}
---
+++ PortfolioProject/ViewComponents/Feature/FeatureList.cs
@@ -0,0 +1,21 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using Business.Constants;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace PortfolioProject.ViewComponents.Feature
+{
+    public class FeatureList:ViewComponent
+    {
+        FeatureManager _featureManager = new FeatureManager(new EfFeatureDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = _featureManager.GetAll().Data;
+            return View(values);
+        }
+    }
+}
