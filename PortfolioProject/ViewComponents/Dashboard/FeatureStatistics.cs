f22b39c36aa1489571a464851ccf570000400788
Oğuz Gül <oguz_gul216@hotmail.com>
2023-07-10 16:59:59 +0300

    NewAdminLayout Edited.

---
+++ PortfolioProject/ViewComponents/Dashboard/FeatureStatistics.cs
@@ -0,0 +1,23 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace PortfolioProject.ViewComponents.Dashboard
+{
+    public class FeatureStatistics:ViewComponent
+    {
+        PortfolioContext context = new PortfolioContext();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = context.Skills.Count();
+            ViewBag.v2 = context.Messages.Where(x=>x.Status==false).Count();
+            ViewBag.v3=context.Messages.Where(x=>x.Status==true).Count();
+            ViewBag.v4=context.Experiences.Count();
+            return View();
+        }
+    }
+}
