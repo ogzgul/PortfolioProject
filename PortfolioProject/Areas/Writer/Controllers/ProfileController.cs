79ddf95e0be68f039c66ae1abbbfc6193c8e07e5
Oğuz Gül <oguz_gul216@hotmail.com>
2023-07-13 16:43:38 +0300

    Profile index view created.

---
+++ PortfolioProject/Areas/Writer/Controllers/ProfileController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioProject.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class ProfileController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(string b)
+        {
+            return View();
+        }
+    }
+}
