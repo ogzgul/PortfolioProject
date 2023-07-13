2043023f2e7c4932189020ccbddf9de7bbd8b396
Oğuz Gül <oguz_gul216@hotmail.com>
2023-07-13 17:15:00 +0300

    Profile controller edited.

--- PortfolioProject/Areas/Writer/Controllers/ProfileController.cs
+++ PortfolioProject/Areas/Writer/Controllers/ProfileController.cs
@@ -2,6 +2,9 @@
 using Entities.Concrete;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using PortfolioProject.Areas.Writer.Models;
+using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace PortfolioProject.Areas.Writer.Controllers
@@ -19,7 +22,35 @@ namespace PortfolioProject.Areas.Writer.Controllers
         public async Task<IActionResult> Index()
         {
             var values = await _userManager.FindByNameAsync(User.Identity.Name);
-            return View(values);
+            UserEditViewModel model = new UserEditViewModel();
+            model.Name = values.Name;
+            model.Surname = values.Surname;
+            model.PictureURL = values.ImageUrl;
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(UserEditViewModel userEditViewModel)
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            if(userEditViewModel.Picture != null)
+            {
+                var resource = Directory.GetCurrentDirectory();
+                var extension = Path.GetExtension(userEditViewModel.Picture.FileName);
+                var imagename = Guid.NewGuid() + extension;
+                var saveLocation = resource + "/wwwroot/userimage/" + imagename;
+                var stream = new FileStream(saveLocation, FileMode.Create);
+                await userEditViewModel.Picture.CopyToAsync(stream);
+                user.ImageUrl = imagename;
+            }
+            user.Name = userEditViewModel.Name;
+            user.Surname = userEditViewModel.Surname;
+            var result = await _userManager.UpdateAsync(user);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Default");
+            }
+
+            return View();
         }
     }
 }
