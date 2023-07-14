60048452b99ab69b2a370f90818b02c3d4aa4c2a
Oğuz Gül <oguz_gul216@hotmail.com>
2023-07-14 15:23:02 +0300

    weather web api added and profile edited.

---
+++ PortfolioProject/Areas/Writer/Controllers/DashboardController.cs
@@ -0,0 +1,43 @@
+﻿using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PortfolioProject.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class DashboardController : Controller
+    {
+        private readonly UserManager<WriterUser> _userManager;
+
+        public DashboardController(UserManager<WriterUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values=await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.v = values.Name + " " + values.Surname;
+
+            //Weather Apisi
+            string api = "5411558b0361031b758080caf25dffd6";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&lang=tr&units=metric&appid="+api;
+            XDocument document = XDocument.Load(connection);
+            ViewBag.v5 = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+
+            //Statistics
+            PortfolioContext context = new PortfolioContext();
+            ViewBag.v1 = 0;
+            ViewBag.v2 = context.Announcements.Count();
+            ViewBag.v3 = 0;
+            ViewBag.v4 = context.Skills.Count();
+            return View();
+        }
+    }
+}
+
+//https://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&lang=tr&units=metric&appid=5411558b0361031b758080caf25dffd6
---
+++ PortfolioProject/Areas/Writer/ViewComponents/Notification.cs
@@ -0,0 +1,17 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace PortfolioProject.Areas.Writer.ViewComponents
+{
+    public class Notification:ViewComponent
+    {
+        AnnouncementManager announcementManager = new AnnouncementManager(new EfAnnouncementDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = announcementManager.GetAll().Data.Take(5).ToList();
+            return View(values);
+        }
+    }
+}
