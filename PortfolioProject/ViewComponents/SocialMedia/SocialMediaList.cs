b62738235d073e53c6e1830f22e98371bd0b2c22
Oğuz Gül <oguz_gul216@hotmail.com>
2023-07-19 12:02:20 +0300

    Admin panel navbar edited.

---
+++ PortfolioProject/ViewComponents/SocialMedia/SocialMediaList.cs
@@ -0,0 +1,16 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioProject.ViewComponents.SocialMedia
+{
+    public class SocialMediaList:ViewComponent
+    {
+        SocialMediaManager _socialMediaManager = new SocialMediaManager(new EfSocialMediaDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = _socialMediaManager.GetAll().Data;
+            return View(values);
+        }
+    }
+}
