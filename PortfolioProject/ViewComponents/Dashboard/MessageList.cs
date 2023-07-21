b9c4a606b0a2792172a1e487c17aa74dc51bebca
Oğuz Gül <oguz_gul216@hotmail.com>
2023-07-21 14:04:23 +0300

    Message image added.

--- PortfolioProject/ViewComponents/Dashboard/MessageList.cs
+++ PortfolioProject/ViewComponents/Dashboard/MessageList.cs
@@ -1,17 +1,29 @@
 ﻿using Business.Concrete;
 using DataAccess.Concrete.EntityFramework;
 using Entities.Concrete;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace PortfolioProject.ViewComponents.Dashboard
 {
     public class MessageList:ViewComponent
     {
         MessageManager messageManager = new MessageManager(new EfMessageDal());
-        public IViewComponentResult Invoke()
+
+        private readonly UserManager<WriterUser> _userManager;
+
+        public MessageList(UserManager<WriterUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
         {
             var values = messageManager.GetAll().Data.Take(5).ToList();
+            var valuess = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.v = valuess.ImageUrl;
             return View(values);
         }
     }
