ad8a2dd1d6a6ea5d2aca248efe079a4c8ba38ee5
Oğuz Gül <oguz_gul216@hotmail.com>
2023-07-21 12:17:15 +0300

    Last5Project edited.

--- PortfolioProject/ViewComponents/Dashboard/Last5Project.cs
+++ PortfolioProject/ViewComponents/Dashboard/Last5Project.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace PortfolioProject.ViewComponents.Dashboard
 {
     public class Last5Project:ViewComponent
     {
+        PortfolioManager _portfolioManager = new PortfolioManager(new EfPortfolioDal());
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values = _portfolioManager.GetAll().Data.Take(5).ToList();
+            return View(values);
         }
     }
 }
